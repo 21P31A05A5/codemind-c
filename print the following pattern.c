9e6a6c80dee940208d29db61e794c62891b14db4
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2022-01-21 13:27:21 -0800

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {     for(j=1;j<=i;j++)     {     printf("%d",j);     }     printf(" ");     }     return 0; }

--- print the following pattern.c
+++ print the following pattern.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-   int i, j, n;
-   scanf("%d", &n);
-   for (i = n; i >= 1; i--) 
-{
-      for (j = 1; j <= i; j++) 
-{
-       printf("%d",j);
-
- }
-      printf("
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    for(j=1;j<=i;j++)
+    {
+    printf("%d",j);
+    }
+    printf("
 ");
-   }
-   return 0;
+    }
+    return 0;
 }
