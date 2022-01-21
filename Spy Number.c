e3dcca4949fe2ef399590429dbca713bbab76bda
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2022-01-21 09:11:14 -0800

    #include<stdio.h> int main() {     int n,rem,sum=0,product=1;     scanf("%d",&n);     while(n!=0)     {     rem=n%10;     sum=sum+rem;     product=product*rem;     n=n/10;     }     if(sum==product)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     }     return 0; }

---
+++ Spy Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,sum=0,product=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+    rem=n%10;
+    sum=sum+rem;
+    product=product*rem;
+    n=n/10;
+    }
+    if(sum==product)
+    {
+        printf("Spy Number");
+    }
+    else
+    {
+        printf("Not Spy Number");
+    }
+    return 0;
+}
